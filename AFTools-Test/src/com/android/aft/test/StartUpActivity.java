6295c5a197a324c5761c7b6cfc52b6350df22846
Fabrice Hesling <fhesling@silicom.fr>
2014-04-01 17:20:49 +0200

    Fix target sdk Add sample test code to use library from test project

--- AFTools-Test/src/com/android/aft/test/StartUpActivity.java
+++ AFTools-Test/src/com/android/aft/test/StartUpActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.android.aft.AFCoreTools.StringTools;
+
 
 public class StartUpActivity extends Activity {
 
@@ -14,6 +16,17 @@ public class StartUpActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         Log.d(TAG, "Start Test App");
+
+        test('c', true);
+        test(';', false);
+    }
+
+    public void test(char c, boolean isAlNum) {
+        boolean res = StringTools.isAlNum(c);
+        if (res ^ isAlNum)
+            Log.d(TAG, "Test failed: char '" + c + "' extected to " + (isAlNum ? "" : "not ") + "be a alpha numeric char");
+        else
+            Log.d(TAG, "Test ok for '" + c + "'");
     }
 
 }
